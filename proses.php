4ef3ac7de13c90560173e78d68f69573adb2f52c
Arliyos17 <yosdilif17@gmail.com>
2023-03-30 09:24:53 +0700

    v1.3

---
+++ proses.php
@@ -0,0 +1,17 @@
+<?php
+$angka1 = $_POST['angka1'];
+$angka2 = $_POST['angka2'];
+$operasi = $_POST['operasi'];
+
+if ($operasi == '+') {
+  $hasil = $angka1 + $angka2;
+} else if ($operasi == '-') {
+  $hasil = $angka1 - $angka2;
+} else if ($operasi == '*') {
+  $hasil = $angka1 * $angka2;
+} else if ($operasi == '/') {
+  $hasil = $angka1 / $angka2;
+}
+
+echo "Hasil: " . $hasil;
+?>
