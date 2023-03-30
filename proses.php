7268ee9f4ecb33eca8c382b052ec514d38abd499
Arliyos17 <yosdilif17@gmail.com>
2023-03-30 09:29:10 +0700

    v1.2.1

---
+++ proses.php
@@ -0,0 +1,24 @@
+<?php
+$angka1 = $_POST['angka1'];
+$angka2 = $_POST['angka2'];
+$operasi = $_POST['operasi'];
+
+switch ($operasi) {
+  case '+':
+    $hasil = $angka1 + $angka2;
+    break;
+  case '-':
+    $hasil = $angka1 - $angka2;
+    break;
+  case '*':
+    $hasil = $angka1 * $angka2;
+    break;
+  case '/':
+    $hasil = $angka1 / $angka2;
+    break;
+  default:
+    $hasil = "Operasi tidak valid";
+}
+
+echo "Hasil: " . $hasil;
+?>
